4926260ae5bd6be15ecae9ccadc0e90ce16e45a9
avlessigroup <avlessijeanbaptiste1@gmail.com>
2024-12-02 13:46:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+if (screen.width <= 500) {
+    $('.menu').hide();
+
+}
+
+
+$('#men').click(function () {
+    $('.menu').toggle();
+})
+
+$('#m').click(function () {
+    $('.menu').toggle();
+})
